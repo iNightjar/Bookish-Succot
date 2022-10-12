fa07f5d003b94cd23926dc504f059e5e58d868df
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-12 02:27:53 +0200

    removing first index def

--- hello/views.py
+++ hello/views.py
@@ -2,11 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 
-
-def index(request):
-    
-    return HttpResponse("Hello, World!")
-
 # overriding index function
 def index(request):
     return render(request, "hello/index.html")
